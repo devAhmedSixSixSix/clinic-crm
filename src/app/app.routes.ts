fa4b6e42791171fe18748358736c6fb377a95174
Dev.Ahmed <dev.ahmed666@gmail.com>
2025-07-30 11:02:33 +0300

    protect routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,77 +1,66 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth-guard';
 
 export const routes: Routes = [
-    {
-        path: "dashboard",
-        loadComponent: () => {
-            return import("./pages/dashboard/dashboard").then((m) => m.Dashboard)
-        },
-
-    },
-    {
-        path: "patient",
-        loadComponent: () => {
-            return import("./pages/patient/patient").then((m) => m.Patient)
-        },
-
-    },
-    {
-        path: "patient/:id",
-        loadComponent: () => {
-            return import("./pages/patient-id/patient-id").then((m) => m.PatientId)
-        },
-
-    },
-    {
-        path: "appointment",
-        loadComponent: () => {
-            return import("./pages/appointment/appointment").then((m) => m.Appointment)
-        },
-
-    },
-    {
-        path: "massages",
-        loadComponent: () => {
-            return import("./pages/massages/massages").then((m) => m.Massages)
-        },
-
-    },
-        {
-        path: "massages/:id",
-        loadComponent: () => {
-            return import("./pages/massages-id/massages-id").then((m) => m.MassagesId)
-        },
-
-    },
-    {
-        path: "profile",
-        loadComponent: () => {
-            return import("./pages/profile/profile").then((m) => m.Profile)
-        },
-
-    },
-
-    {
-        path: "signup",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./pages//sign-up/sign-up").then((m) => m.SignUp)
-        },
-
-    },
-    {
-        path: "",
-        pathMatch: "full",
-        loadComponent: () => {
-            return import("./pages/sign-in/sign-in").then((m) => m.SignIn)
-        },
-
-    },
-    {
-        path: "forget-password",
-        loadComponent: () => {
-            return import("./pages/forget-password/forget-password").then((m) => m.ForgetPassword)
-        },
-
-    }
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard').then((m) => m.Dashboard),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'patient',
+    loadComponent: () =>
+      import('./pages/patient/patient').then((m) => m.Patient),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'patient/:id',
+    loadComponent: () =>
+      import('./pages/patient-id/patient-id').then((m) => m.PatientId),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'appointment',
+    loadComponent: () =>
+      import('./pages/appointment/appointment').then((m) => m.Appointment),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'massages',
+    loadComponent: () =>
+      import('./pages/massages/massages').then((m) => m.Massages),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'massages/:id',
+    loadComponent: () =>
+      import('./pages/massages-id/massages-id').then((m) => m.MassagesId),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./pages/profile/profile').then((m) => m.Profile),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'signup',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/sign-up/sign-up').then((m) => m.SignUp),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/sign-in/sign-in').then((m) => m.SignIn),
+  },
+  {
+    path: 'forget-password',
+    loadComponent: () =>
+      import('./pages/forget-password/forget-password').then(
+        (m) => m.ForgetPassword
+      ),
+  },
 ];
